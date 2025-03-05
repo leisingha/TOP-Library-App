acf58ce791a820fcf6de3a9ad9e80a6dde8f89b4
leisingha <120764124+leisingha@users.noreply.github.com>
2025-03-05 01:06:28 -0500

    Generate table using js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const library  = [];
 
-function Book(title, author, pages, desc){
+function Book(title, author, pages, desc, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.desc = desc;
     this.id = crypto.randomUUID();
+    this.read = read;
 }
 
 function addBooktoLibrary(title, author, pages, desc){
@@ -13,20 +14,44 @@ function addBooktoLibrary(title, author, pages, desc){
     library.push(bookObj);
 }
 
-addBooktoLibrary('Harry Potter', 'JK Rowling', 500, 'Book about wizards and magic.' );
+//Dummy Data for Book Library
+addBooktoLibrary('Harry Potter', 'JK Rowling', 500, 'Book about wizards and magic.', true );
 
-addBooktoLibrary('Percy Jackson', 'Aston Pierce', 400, 'Book about greek gods and mythology.' );
+addBooktoLibrary('Percy Jackson', 'Aston Pierce', 400, 'Book about greek gods and mythology.', true );
 
-addBooktoLibrary('Goose Bumps', 'Bob Parson', 400, 'Book about paranormal activities.' );
+addBooktoLibrary('Goose Bumps', 'Bob Parson', 400, 'Book about paranormal activities.', false );
 
 function displayBooks(){
     const display = document.querySelector('.display');
-    library.forEach( (element) =>{
-        let bookTitle = document.createElement('div');
-        bookTitle.textContent = element.title;
-        display.appendChild(bookTitle);
+    const table = document.createElement('table');
+    const caption = document.createElement('caption');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+
+    display.append(table);
+    table.appendChild(caption);
+    caption.textContent = 'Books and the information';
+    table.appendChild(thead);
+    const headers = ['Book', 'Author', 'Pages', 'Description', 'ID', 'Read?']
+    headers.forEach(element => {
+        const th = document.createElement('th');
+        th.textContent = element;
+        thead.appendChild(th);
+    });
+    table.appendChild(tbody);
+    library.forEach( (book) =>{
+        const tr = document.createElement('tr');
+        tbody.appendChild(tr);
+        for (const property in book){
+            const td = document.createElement('td');
+            td.textContent = book[property];
+            tr.appendChild(td);
+        }
     })
 }
+
+
+
 
 displayBooks();
 
